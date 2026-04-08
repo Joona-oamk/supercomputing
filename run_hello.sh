98f910a5caa0729d8bfc416f9758aaa05dccbc5c
Joona Leskinen <>
2026-04-08 17:45:25 +0300

    add bash

---
+++ run_hello.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --account=project_2018026
+#SBATCH --job-name=hello
+#SBATCH --output=log_file.txt
+#SBATCH --time=00:05:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --partition=test
+
+module load python-data
+python temp.py
